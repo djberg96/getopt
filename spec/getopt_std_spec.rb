8e95e06483ae0c023de18291f98361c2552a324d
Daniel Berger <djberg96@gmail.com>
2022-05-18 04:29:57 -0400

    Use described_class in Std specs.

--- spec/getopt_std_spec.rb
+++ spec/getopt_std_spec.rb
@@ -3,12 +3,11 @@
 ###################################################################
 # test_getopt_std.rb
 #
-# Test suite for the Getopt::Std class. You should run this test
+# Test suite for the Getopt::described_class class. You should run this test
 # via the 'rake test' task.
 ###################################################################
 require 'rspec'
 require 'getopt/std'
-include Getopt
 
 RSpec.describe Getopt::Std do
   after do
@@ -16,72 +15,72 @@ RSpec.describe Getopt::Std do
   end
 
   example 'version' do
-    expect(Std::VERSION).to eq('1.6.0')
-    expect(Std::VERSION).to be_frozen
+    expect(described_class::VERSION).to eq('1.6.0')
+    expect(described_class::VERSION).to be_frozen
   end
 
   example 'getopts basic functionality' do
-    expect(Std).to respond_to(:getopts)
-    expect{ Std.getopts('ID') }.not_to raise_error
-    expect(Std.getopts('ID')).to be_kind_of(Hash)
+    expect(described_class).to respond_to(:getopts)
+    expect{ described_class.getopts('ID') }.not_to raise_error
+    expect(described_class.getopts('ID')).to be_kind_of(Hash)
   end
 
   example 'getopts with separated switches' do
     ARGV.push('-I', '-D')
-    expect(Std.getopts('ID')).to eq({'I' => true, 'D' => true})
+    expect(described_class.getopts('ID')).to eq({'I' => true, 'D' => true})
   end
 
   # Inspired by RF bug #23477
   example 'getopts with arguments that match switch are ok' do
     ARGV.push('-d', 'd')
-    expect(Std.getopts('d:')).to eq({'d' => 'd'})
+    expect(described_class.getopts('d:')).to eq({'d' => 'd'})
 
     ARGV.push('-d', 'ad')
-    expect(Std.getopts('d:')).to eq({'d' => 'ad'})
+    expect(described_class.getopts('d:')).to eq({'d' => 'ad'})
 
     ARGV.push('-a', 'ad')
-    expect(Std.getopts('d:a:')).to eq({'a' => 'ad'})
+    expect(described_class.getopts('d:a:')).to eq({'a' => 'ad'})
 
     ARGV.push('-a', 'da')
-    expect(Std.getopts('d:a:')).to eq({'a' => 'da'})
+    expect(described_class.getopts('d:a:')).to eq({'a' => 'da'})
 
     ARGV.push('-a', 'd')
-    expect(Std.getopts('d:a:')).to eq({'a' => 'd'})
+    expect(described_class.getopts('d:a:')).to eq({'a' => 'd'})
 
     ARGV.push('-a', 'dad')
-    expect(Std.getopts('d:a:')).to eq({'a' => 'dad'})
+    expect(described_class.getopts('d:a:')).to eq({'a' => 'dad'})
 
     ARGV.push('-d', 'd', '-a', 'a')
-    expect(Std.getopts('d:a:')).to eq({'d' => 'd', 'a' => 'a'})
+    expect(described_class.getopts('d:a:')).to eq({'d' => 'd', 'a' => 'a'})
   end
 
   example 'getopts with joined switches' do
     ARGV.push('-ID')
-    expect(Std.getopts('ID')).to eq({'I' => true, 'D' => true})
+    expect(described_class.getopts('ID')).to eq({'I' => true, 'D' => true})
   end
 
   example 'getopts with separated switches and mandatory argument' do
     ARGV.push('-o', 'hello', '-I', '-D')
-    expect(Std.getopts('o:ID')).to eq({'o' => 'hello', 'I' => true, 'D' => true})
+    expect(described_class.getopts('o:ID')).to eq({'o' => 'hello', 'I' => true, 'D' => true})
   end
 
   example 'getopts with joined switches and mandatory argument' do
     ARGV.push('-IDo', 'hello')
-    expect(Std.getopts('o:ID')).to eq({'o' => 'hello', 'I' => true, 'D' => true})
+    expect(described_class.getopts('o:ID')).to eq({'o' => 'hello', 'I' => true, 'D' => true})
   end
 
   example 'getopts with no arguments' do
-    expect{ Std.getopts('ID') }.not_to raise_error
-    expect(Std.getopts('ID')).to eq({})
-    expect(Std.getopts('ID')['I']).to be_nil
-    expect(Std.getopts('ID')['D']).to be_nil
+    expect{ described_class.getopts('ID') }.not_to raise_error
+    expect(described_class.getopts('ID')).to eq({})
+    expect(described_class.getopts('ID')['I']).to be_nil
+    expect(described_class.getopts('ID')['D']).to be_nil
   end
 
   # If a switch that accepts an argument appears more than once, the values
   # are rolled into an array.
   example 'getopts with switch repeated' do
     ARGV.push('-I', '-I', '-o', 'hello', '-o', 'world')
-    expect(Std.getopts('o:ID')).to eq({'o' => ['hello', 'world'], 'I' => true})
+    expect(described_class.getopts('o:ID')).to eq({'o' => ['hello', 'world'], 'I' => true})
   end
 
   # EXPECTED ERRORS
@@ -90,37 +89,37 @@ RSpec.describe Getopt::Std do
     msg = "cannot use switch '-d' as argument to another switch"
 
     ARGV.push('-d', '-d')
-    expect{ Std.getopts('d:a:') }.to raise_error(Getopt::Std::Error)
+    expect{ described_class.getopts('d:a:') }.to raise_error(Getopt::Std::Error)
 
     ARGV.push('-d', '-a')
-    expect{ Std.getopts('d:a:') }.to raise_error(Getopt::Std::Error)
+    expect{ described_class.getopts('d:a:') }.to raise_error(Getopt::Std::Error)
 
     ARGV.push('-a', '-d')
-    expect{ Std.getopts('d:a:') }.to raise_error(Getopt::Std::Error)
+    expect{ described_class.getopts('d:a:') }.to raise_error(Getopt::Std::Error)
 
     ARGV.push('-d', '-d')
-    expect{ Std.getopts('d:a:') }.to raise_error(Getopt::Std::Error, msg)
+    expect{ described_class.getopts('d:a:') }.to raise_error(Getopt::Std::Error, msg)
   end
 
   example 'getopts raises expected errors if argument is missing' do
     ARGV.push('-ID')
-    expect{ Std.getopts('I:D') }.to raise_error(Std::Error)
+    expect{ described_class.getopts('I:D') }.to raise_error(Getopt::Std::Error)
 
     ARGV.push('-ID')
-    expect{ Std.getopts('ID:') }.to raise_error(Std::Error)
+    expect{ described_class.getopts('ID:') }.to raise_error(Getopt::Std::Error)
   end
 
   example 'getopts raises expected errors if there are extra arguments' do
     ARGV.push('-I', '-D', '-X')
-    expect{ Std.getopts('ID') }.to raise_error(Std::Error)
+    expect{ described_class.getopts('ID') }.to raise_error(Getopt::Std::Error)
 
     ARGV.push('-IDX')
-    expect{ Std.getopts('ID') }.to raise_error(Std::Error, "invalid option 'X'")
+    expect{ described_class.getopts('ID') }.to raise_error(Getopt::Std::Error, "invalid option 'X'")
   end
 
   example 'getopts raises expected errors with invalid or no arguments' do
-    expect{ Std.getopts }.to raise_error(ArgumentError)
-    expect{ Std.getopts(0) }.to raise_error(NoMethodError)
-    expect{ Std.getopts(nil) }.to raise_error(NoMethodError)
+    expect{ described_class.getopts }.to raise_error(ArgumentError)
+    expect{ described_class.getopts(0) }.to raise_error(NoMethodError)
+    expect{ described_class.getopts(nil) }.to raise_error(NoMethodError)
   end
 end
